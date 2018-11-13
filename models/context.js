8fd5bc3892e7ab9fd7e516d5672f16578b467aa7
Nickolas Fuentes <>
2018-11-12 18:22:09 -0600

    Restructured Context model and migration

--- models/context.js
+++ models/context.js
@@ -1,9 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Context = sequelize.define('Context', {
-    name: DataTypes.STRING
-  }, {});
-  Context.associate = function(models) {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  });
+
+  Context.associate = function (models) {
     // associations can be defined here
   };
   return Context;
