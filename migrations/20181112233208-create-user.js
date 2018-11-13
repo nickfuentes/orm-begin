d83c796d3d6277761c7d600e698a22b14a181557
Nickolas Fuentes <>
2018-11-12 18:18:45 -0600

    Restructured User models and migration

--- migrations/20181112233208-create-user.js
+++ migrations/20181112233208-create-user.js
@@ -9,10 +9,12 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
